7c6a0d662d972365c87dafaeed4fb97618924a0d
Stanley Masinde <stanmasinde@gmail.com>
2025-01-02 19:50:39 +0300

    feat: config gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: './docs',
-  },
-  plugins: [vue()]
+  plugins: [vue()],
+  base: 'pwa-generator'
 })
