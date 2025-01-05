a106ad5b31424e413f9cb52c6f9882eb8a54f0a5
Stanley Masinde <stanmasinde@gmail.com>
2025-01-05 19:27:43 +0300

    ft: add eslint and it's config

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import stylistic from '@stylistic/eslint-plugin'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,vue}'] },
+  {
+    ignores: ['dist/*'],
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  stylistic.configs.customize({}),
+  ...pluginVue.configs['flat/essential'],
+]
