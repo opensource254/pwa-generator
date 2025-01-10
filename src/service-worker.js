9f28ab30b5efffa9ac5dc6f0375632c07590e6f7
Stanley Masinde <stanmasinde@gmail.com>
2025-01-10 15:46:18 +0300

    chore: update the manifest

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,28 @@
-// const CACHE_NAME = `pwa-cache-${process.env.BUILD_HASH}`
+// eslint-disable-next-line no-undef
+const CACHE_NAME = `cache-${process.env.BUILD_HASH}`
 // Install event - Cache static assets
-self.addEventListener('install', () => {
-  self.skipWaiting()
-  console.log('The install event will be here')
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        cache.addAll([
+          '/',
+        ])
+      }),
+  )
 })
 
 // Activate event - Clean up old caches
 self.addEventListener('activate', (event) => {
+  const cacheAllowList = [CACHE_NAME]
   event.waitUntil(
-    self.clients.claim(),
+    caches.keys().then((allCaches) => {
+      allCaches.forEach(async (cache) => {
+        if (!cacheAllowList.includes(cache)) {
+          await caches.delete(cache)
+        }
+      })
+    }),
   )
 })
 
